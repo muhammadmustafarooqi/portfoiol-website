8d6c0cc515567bd7cb118b96fe140e715ab55a49
Muhammad Mustafa Hussain <140386879+muhammadmustafarooqi@users.noreply.github.com>
2024-11-13 10:53:21 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*=============== SHOW MENU ===============*/
+
+// JavaScript for toggling the navbar menu
+const navLinks = document.querySelector('.nav-links')
+function onToggleMenu(e) {
+   e.name = e.name === 'menu' ? 'close' : 'menu'
+   navLinks.classList.toggle('top-[9%]')
+}
+  
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD BLUR HEADER ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
